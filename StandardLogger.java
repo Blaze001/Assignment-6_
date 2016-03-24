2a6400c0eef9ddbd3d9719d7bb3983a182cd7bb2
Blaze001 <mvaness02@gmail.com>
2016-03-23 23:15:33 -0400

    StandardLogger.java
    The source code for StandardLogger.java as of Step 5 part b.

---
+++ StandardLogger.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * @author Mitchell VanEss
+ *
+ */
+public abstract class StandardLogger implements Logger1 {
+
+	/**
+	 * 
+	 */
+	public StandardLogger() {
+		// TODO Auto-generated constructor stub
+		setLevel(Level.WARN);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
