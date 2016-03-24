192cf334716b82b6623e554afbe93fcfd7898794
Blaze001 <mvaness02@gmail.com>
2016-03-24 19:54:55 -0400

    Logger1 Updated Source Code

--- Logger1.java
+++ Logger1.java
@@ -1,4 +1,4 @@
-/**
+\**
  * 
  */
 
@@ -8,30 +8,35 @@
  */
 
 public interface Logger1 {
-	void getLevel(String level);
+	public Level getLevel();
 		// TODO Auto-generated method stub
 		
-	 void setLevel(Level level);
+	public void setLevel(Level level);
 		
 	/**
 	 * The info method prints the message when the level is set to INFO.
 	 * @param text
 	 */
-	void info(String text);
+	public void info(String text);
 	/**
 	 * The debug method prints the message when the level is INFO or DEBUG.
 	 * @param text
 	 */
-	void debug(String text);
+	public void debug(String text);
 	/**
 	 * The warn method prints the message when the level is INFO, DEBUG or WARN.
 	 * @param text
 	 */
-	void warn(String text);
+	public void warn(String text);
 	/**
 	 * The error method always prints the message when called.
 	 * @param text
 	 */
-	void error(String text);
+	public void error(String text);
 	
 }
+
+
+
+
+	
